ab5a2b7bac5ee190add06877d98cb2615ac1135a
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-12 11:34:20 -0600

    Initial Commit

---
+++ Buffetec/BuffetecApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuffetecApp.swift
+//  Buffetec
+//
+//  Created by Benjamin Belloeil on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BuffetecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
