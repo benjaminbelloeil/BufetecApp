a0eb843c37ac1527cb3b7b38c9adb1a044fa67a0
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-13 13:37:48 -0600

    Advanced Login page

---
+++ Buffetec/View/ForgotPassword.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPassword.swift
+//  Buffetec
+//
+//  Created by Benjamin Belloeil on 8/13/24.
+//
+
+import SwiftUI
+
+struct ForgotPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPassword()
+}
---
+++ Buffetec/View/Signup.swift
@@ -0,0 +1,18 @@
+//
+//  Signup.swift
+//  Buffetec
+//
+//  Created by Benjamin Belloeil on 8/13/24.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Signup()
+}
