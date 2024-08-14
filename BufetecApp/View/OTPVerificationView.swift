fbe8e230b8ae44c1b077d3c06002e9e6fe2f46d7
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-14 13:38:49 -0600

    feat: Added OTPVerificationView and OTPView
    Add OTPVerificationView and OTPView to the project.
    These views will be used for OTP verification in the login process.

---
+++ BufetecApp/View/OTPVerificationView.swift
@@ -0,0 +1,18 @@
+//
+//  OTPVerificationView.swift
+//  BufetecApp
+//
+//  Created by Benjamin Belloeil on 8/14/24.
+//
+
+import SwiftUI
+
+struct OTPVerificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OTPVerificationView()
+}
---
+++ BufetecApp/View/OTPView.swift
@@ -0,0 +1,18 @@
+//
+//  OTPView.swift
+//  BufetecApp
+//
+//  Created by Benjamin Belloeil on 8/14/24.
+//
+
+import SwiftUI
+
+struct OTPView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OTPView()
+}
