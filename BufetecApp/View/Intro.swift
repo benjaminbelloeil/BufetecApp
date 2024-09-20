4e38f104091e0f28b0f6a5bcd0b5a4fb1cc1e754
Benjamin <benjaminbelloeil03@gmail.com>
2024-09-19 18:11:02 -0600

    feat: Added new image assets for the intro screens in the BufetecApp. The images are added to the respective image sets with different scales.

---
+++ BufetecApp/View/Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Intro.swift
+//  BufetecApp
+//
+//  Created by Benjamin Belloeil on 9/17/24.
+//
+
+import SwiftUI
+
+struct Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Intro()
+}
---
+++ BufetecApp/View/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  BufetecApp
+//
+//  Created by Benjamin Belloeil on 9/17/24.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
