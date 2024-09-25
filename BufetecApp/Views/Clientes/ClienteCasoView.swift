dffb9bcaf912e731744b9e562f573ab78721d3db
Edsel De Jesus Cisneros Bautista <111032109+Edsel675@users.noreply.github.com>
2024-09-24 22:13:22 -0600

    miscasos

---
+++ BufetecApp/Views/Clientes/ClienteCasoView.swift
@@ -0,0 +1,18 @@
+//
+//  ClienteCasoView.swift
+//  BufetecApp
+//
+//  Created by Edsel Cisneros Bautista on 24/09/24.
+//
+
+import SwiftUI
+
+struct ClienteCasoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClienteCasoView()
+}
