ef632fef797a56d8606f9304f4bf738bf887da0b
AlexHdz357 <117100191+AlexHdz357@users.noreply.github.com>
2024-09-30 12:19:52 -0600

    hola?

--- BufetecApp/Models/Cliente.swift
+++ BufetecApp/Models/Cliente.swift
@@ -1,12 +1,22 @@
-struct Client: Identifiable {
-    var id: String
-    var name: String
-    var caseType: String
-    var caseStatus: String
-    var lawyerAssigned: String
-    var startDate: String
-    var nextHearingDate: String
-    var email: String
-    var phoneNumber: String
-    var address: String
+
+import SwiftUI
+
+struct Cliente: Identifiable {
+    let id = UUID()
+    let name: String
+    let caseType: String
+    let status: String
+    
+    var statusColor: Color {
+        switch status {
+        case "Activo":
+            return .green
+        case "En espera":
+            return .orange
+        case "Cerrado":
+            return .red
+        default:
+            return .gray
+        }
+    }
 }
--- BufetecApp/Views/Clientes/ClienteListView.swift
+++ BufetecApp/Views/Clientes/ClienteListView.swift
@@ -74,26 +74,6 @@ struct ClienteCard: View {
     }
 }
 
-struct Cliente: Identifiable {
-    let id = UUID()
-    let name: String
-    let caseType: String
-    let status: String
-    
-    var statusColor: Color {
-        switch status {
-        case "Activo":
-            return .green
-        case "En espera":
-            return .orange
-        case "Cerrado":
-            return .red
-        default:
-            return .gray
-        }
-    }
-}
-
 struct ClienteDetailView: View {
     var cliente: Cliente
     
